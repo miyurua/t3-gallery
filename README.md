b433d7e28949795d67046f362c94dd990d9d4d5c
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-07-30 00:05:06 +0530

    upload thing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## TODO
 
-- [] Make it deploy (vercel)
+- [x] Make it deploy (vercel)
 - [] Scaffold basic ui with mock data
 - [] Actually set up a db (vercel postgres)
 - [] Attach a db to UI
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/5616d15c-17d4-4cb4-95ac-ae9d871c6c6b-rv9e2f.jpeg",
+  "https://utfs.io/f/ae7c3b84-dee4-4567-b455-c052259fb2a7-rv9e2d.jpeg",
+  "utfs.io/f/5616d15c-17d4-4cb4-95ac-ae9d871c6c6b-rv9e2f.jpeg",
+  "https://utfs.io/f/91ff25f3-f00b-41af-ae86-dc3e458f6d7a-1u06f.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((img) => (
+          <div key={img.id} className="w-1/4 p-4">
+            <img src={img.url} />
+          </div>
+        ))}
+      </div>
       Hello Gallery in progress
     </main>
   );
